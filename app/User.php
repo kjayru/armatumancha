4196a5b1f167101ff4326fc01a8c204bdd978cc1
kjayru <wiltinoco@gmail.com>
2018-11-16 14:18:26 -0500

    class-models

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email',
     ];
 
     /**
@@ -27,4 +27,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function groups(){
+        return $this->belongsToMany('App\Group');
+    }
+
+    public function role(){
+        return $this->belongsTo('App\Role');
+    }
 }
