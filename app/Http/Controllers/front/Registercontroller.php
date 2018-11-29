8b536dd74d920db614d523ab79b3e2c36de0e4b5
Wile <wiltinoco@gmail.com>
2018-11-29 15:26:22 -0500

    parametro

--- app/Http/Controllers/front/Registercontroller.php
+++ app/Http/Controllers/front/Registercontroller.php
@@ -88,7 +88,7 @@ class RegisterController extends Controller
 
 
         $notification = array(
-            'notification' => 'codigo-seguridad',
+            'notification' => 'invitacion',
             'users' => array($user->id)
         );
 
